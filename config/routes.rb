8001c6a954329a82a2efffc5a8e6dcfa31f868ad
ivana <ivananl@yahoo.com>
2021-05-27 20:08:22 +0200

    Add new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  
-root "articles#index"
+    
+    
+  root "articles#index"
 
+resources :articles
   get "/articles/:id", to: "articles#show"
   get "/articles", to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
